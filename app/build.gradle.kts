ad52201462c30087fb4122fdba29e312a9b5f0c1
Jay Mee <jamied_uk@users.noreply.github.com>
2026-06-03 01:13:40 +0100

    Fix: Use Theme.MaterialComponents theme, add material library dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
