07c5b507afb9a7fc2905a5bd292751dd6bc55df6
Jay Mee <jamied_uk@users.noreply.github.com>
2026-06-03 01:14:04 +0100

    Fix theme: use AppCompat parent, add appcompat dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
